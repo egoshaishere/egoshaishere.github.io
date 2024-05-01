7cfb705714af3a584941decc71fbe277ef6d7ce4
egoshaishere <109945006+egoshaishere@users.noreply.github.com>
2024-05-01 04:32:03 +0300

    Add files via upload

---
+++ assets/Background_5--7xhii0f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as c}from"./index-Li-qLaqc.js";const s={},n={width:"440",height:"440",viewBox:"0 0 440 440",fill:"none",xmlns:"http://www.w3.org/2000/svg"},_=c("rect",{width:"440",height:"440",fill:"#985cfa"},null,-1),r=[_];function a(i,h){return t(),o("svg",n,r)}const d=e(s,[["render",a]]);export{d as default};
