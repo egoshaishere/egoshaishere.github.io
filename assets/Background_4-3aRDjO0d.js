5e2232e974de7e3bf32c4713fbc80f1f3c128cf9
egoshaishere <109945006+egoshaishere@users.noreply.github.com>
2024-05-01 06:27:16 +0300

    Add files via upload

---
+++ assets/Background_4-3aRDjO0d.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,a as c}from"./index-BhIl2KIB.js";const s={},n={width:"440",height:"440",viewBox:"0 0 440 440",fill:"none",xmlns:"http://www.w3.org/2000/svg"},_=c("rect",{width:"440",height:"440",fill:"#76ebc8"},null,-1),r=[_];function a(i,h){return t(),o("svg",n,r)}const d=e(s,[["render",a]]);export{d as default};
